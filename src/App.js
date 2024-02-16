3358e4c571c49ad1a1e63874fd0d337a7ab2ad52
wtplant <wtplant@gmail.com>
2024-02-16 18:20:07 -0500

    SRC Folder

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SignIn from './SignIn';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SignIn />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
